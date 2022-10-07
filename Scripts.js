842789f62903d29ea87d781fbd2cd9a0147ae770
sriishti <srishtinandy1@gmail.com>
2022-10-07 12:43:21 +0200

    Cookie Scripts
    started some of the scripts involving cookies for log in page :apple:

--- Scripts.js
+++ Scripts.js
@@ -10,3 +10,29 @@ function LoggedIn(loggedIn) {
 }
 
 LoggedIn(false);
+
+function setCookie(cName, cValue){
+    document.cookie = cName + "=" + cValue +";path=/";
+}
+
+function getCookie(cName) {
+    let name = cName + "=";
+
+    //can handle cookies with special chars
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let charArray = document.cookie.split(';');
+    
+    //iterates through char array of each cookie string
+    for (let i = 0; i < charArray.length; i++){
+        let c = charArray[i];
+        
+        //
+        while (c.charAt(0) == ''){
+            c = c.substring(1);
+        }
+        if (c.indexOf(name)==0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
