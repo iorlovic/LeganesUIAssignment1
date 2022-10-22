4e1eb6f2b891e11dbe03f9c04e7997e3cc879eda
nateyboy12 <natecourchesne@gmail.com>
2022-10-22 19:00:44 +0200

    Cookies/Screen Switching

--- Scripts.js
+++ Scripts.js
@@ -1,26 +1,53 @@
+function logIn() {
+    console.log("User Cookie: " + getCookie("user"));
+    console.log("User Form: " + document.getElementById("user").value);
+    console.log("Password Cookie: " + getCookie("password"));
+    console.log("Password Form: " + document.getElementById("password").value);
+    if (!checkCookie("user", document.getElementById("user").value)) {
+        alert("Username does not exist")
+        return false;
+    }
+    if (!checkCookie("password", document.getElementById("password").value)) {
+        alert("Wrong username/password combination");
+        return false;
+    }
+    console.log("Correct credentials")
+    setCookie("loggedIn", true, 1);
+    return true;
+}
 
+function logOut() {
+    setCookie("loggedIn", false, 1);
+}
 
 function isLoggedIn() {
-    if (checkCookie("user")) {
+    const WELCOMEMESSAGE = document.getElementById("logInMessage");
+    const CREDS = document.getElementById("creds");
+    const LOGOUT = document.getElementById("logOut");
+    if (checkCookie("loggedIn", "true")) {
         WELCOMEMESSAGE.innerHTML = "";
+        CREDS.hidden = true;
+        LOGOUT.hidden = false;
     }
-    else {
+    else if (checkCookie("loggedIn", "false")){
         WELCOMEMESSAGE.innerHTML = "If you're new here, don't forget to create an account to save your favorite songs!";
+        CREDS.hidden = false;
+        LOGOUT.hidden = true;
     }
 }
 
 function createNewAccount() {
-    console.log(getCookie("user"));
+    console.log("Before: " + getCookie("user"));
     setCookie("user", document.getElementById("user").value, 1);
-    console.log(document.getElementById("user").value);
-    console.log(getCookie("user"));
+    setCookie("password", document.getElementById("password").value, 1);
+    console.log("After: " + getCookie("user"));
 }
 
 function setCookie(cName, cValue, days) {
     const date = new Date();
     date.setTime(date.getTime() + (days*24*60*60*1000));
     let expires = "expires="+ date.toUTCString();
-    document.cookie = cName + "=" + cValue + ";" + expires + ";path=/";
+    document.cookie = cName + "=" + cValue + ";" + expires + ";SameSite=None;path=/;Secure";
 }
 
 function getCookie(cName) {
@@ -52,10 +79,19 @@ function checkCookie(cName) {
     return false;
 }
 
-function validateForm() {
-    if(true) {
-        createNewAccount();
+function checkCookie(cName, entry) {
+    if (getCookie(cName) == entry) {
         return true;
     }
     return false;
+}
+
+function validateForm() {
+    if(false) {
+        alert("Email already in use");
+        return false;
+    }
+    console.log("Form: " + document.getElementById("user").value);
+    createNewAccount();
+    return true;
 }
